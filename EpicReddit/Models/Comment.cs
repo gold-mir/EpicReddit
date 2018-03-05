79f4f162a2628d873948ed5803d25e5f556de74d
Ainur Dzhaianbaeva and Miranda Keith <ainur+miranda@>
2018-03-05 13:47:59 -0800

    setup comment class

--- EpicReddit/Models/Comment.cs
+++ EpicReddit/Models/Comment.cs
@@ -4,11 +4,50 @@ namespace EpicReddit.Models
 {
     public class Comment
     {
-        public string UserName;
-        public string Body;
-        public int postID;
-        public int parentCommentID;
-        public int id;
+        private string _username;
+        private string _body;
+        private int _postID;
+        private int _parentCommentID;
+        private int _id;
+
+        public Comment(string body, string username, int postID, int parentCommentID = -1)
+        {
+            _body = body;
+            _username = username;
+            _postID = postID;
+            _parentCommentID = parentCommentID;
+            _id = -1;
+        }
+
+        public int GetID()
+        {
+            return _id;
+        }
+
+        public string GetUsername()
+        {
+            return _username;
+        }
+
+        public string GetBody()
+        {
+            return _body;
+        }
+
+        public int GetPostID()
+        {
+            return _postID;
+        }
+
+        public int GetParentCommentID()
+        {
+            return _parentCommentID;
+        }
+
+        public void Edit(string newBody)
+        {
+            throw new NotImplementedException();
+        }
 
         public Post GetParentPost()
         {
@@ -21,6 +60,21 @@ namespace EpicReddit.Models
         }
 
         public Comment[] GetChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Comment[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Comment GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DeleteAll()
         {
             throw new NotImplementedException();
         }
